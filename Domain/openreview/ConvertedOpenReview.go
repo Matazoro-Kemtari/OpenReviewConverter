5851766d1bd94ce6be64d49ef187eb8f9baccdb1
pie <k_hirano@wadass.com>
2021-07-14 15:20:50 +0900

    Add: オープンレビュー変換

---
+++ Domain/openreview/ConvertedOpenReview.go
@@ -0,0 +1,40 @@
+package openreview
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type ConvertedOpenReview struct{}
+
+func NewConvertedOpenReview() *ConvertedOpenReview {
+	return &ConvertedOpenReview{}
+}
+
+func (c *ConvertedOpenReview) Convert(sources []string) ([]string, error) {
+	if len(sources) == 0 {
+		return nil, fmt.Errorf("変換対象がありません")
+	}
+
+	var canSkipLine bool
+	rexM00 := regexp.MustCompile(`^M00$`)
+	rexM01 := regexp.MustCompile(`^M01$`)
+	rexM99 := regexp.MustCompile(`^\(M99\)$`)
+	var res []string
+	for _, line := range sources {
+		if rexM00.MatchString(line) {
+			continue
+		}
+
+		if !canSkipLine && rexM01.MatchString(line) {
+			res = append(res, line)
+			canSkipLine = true
+		} else if canSkipLine && rexM99.MatchString(line) {
+			res = append(res, line)
+			canSkipLine = false
+		} else if !canSkipLine {
+			res = append(res, line)
+		}
+	}
+	return res, nil
+}
